b7f90ad1cd0bd3b940f6c28cb92ea7d1e4be1309
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-05-15 17:47:21 +0300

    renamed file NootFoundPage

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,101 @@
+import { NavLink, useParams } from 'react-router-dom';
+import { Person } from '../types';
+import { Loader } from './Loader';
+
+type Props = {
+  people: Person[];
+  error: boolean;
+  loading: boolean;
+};
+
+export const PeopleTable = ({ people, error, loading }: Props) => {
+  const { slug } = useParams();
+
+  const getPersonLink = (name: string | null) => {
+    if (!name) {
+      return '-';
+    }
+
+    const person = people.find(p => p.name === name);
+
+    if (person) {
+      return (
+        <NavLink
+          to={`/people/${person.slug}`}
+          className={person.sex === 'f' ? 'has-text-danger' : ''}
+        >
+          {name}
+        </NavLink>
+      );
+    }
+
+    return name;
+  };
+
+  return (
+    <div className="container">
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
+
+          {error && !loading && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!error && !loading && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!loading && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {people.map(person => (
+                  <tr
+                    key={person.slug}
+                    data-cy="person"
+                    className={
+                      person.slug === String(slug)
+                        ? 'has-background-warning'
+                        : ''
+                    }
+                  >
+                    <td>
+                      <NavLink
+                        to={`/people/${person.slug}`}
+                        className={person.sex === 'f' ? 'has-text-danger' : ''}
+                      >
+                        {person.name}
+                      </NavLink>
+                    </td>
+
+                    <td>{person.sex}</td>
+                    <td>{person.born}</td>
+                    <td>{person.died}</td>
+                    <td>{getPersonLink(person.motherName)}</td>
+                    <td>{getPersonLink(person.fatherName)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
